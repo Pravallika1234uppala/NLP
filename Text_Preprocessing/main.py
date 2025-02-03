c318f7597c188a3489e9e48b7e26094973d7f582
Pravallika Uppala <123845793+Pravallika1234uppala@users.noreply.github.com>
2025-02-03 11:33:28 -0600

    Add files via upload

---
+++ Text_Preprocessing/main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from gensim.models import KeyedVectors
+import gdown
+import os
+
+#Loading the traioned model:
+@st.cache_resource
+def load_word2vec():
+    model_url="https://drive.google.com/file/d/1NXe6o5kEluV86RWO7nEV5WUQnS6u6_EV/view?usp=sharing"
+    model_file="word2vec_model.model"
+
+    if not os.path.exists(model_file):
+        gdown.download(model_url, model_file, quiet=False)
+
+    # Load the model
+    return KeyedVectors.load(model_file, mmap='r')
+
+model = load_word2vec()
+
+#starting streamlit app-
+#Giving title to app
+st.title('Word and Similar Word Prediction')
+
+#word Input:
+
+input_word = st.text_input("Enter a word:")
+
+if input_word:
+    try:
+        vec = model[input_word]
+        prediction = model.most_similar([vec], topn=5)
+
+        similar_words = [wrd for wrd, _ in prediction]
+
+        st.write("Most similar words:")
+        for similar_word in similar_words:
+            st.write(f"{similar_word}")
+    
+    except KeyError:
+        st.write("Word not found in the vocabulary. Try another word!")
